79c22dc490b7427688eee702d7c33cbc3c1cf392
andyzen619 <andyzen619@gmail.com>
2019-10-18 18:37:52 +0000

    Added recursive sum functionality

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-function sumItems(array) {
+/**
+ * Recursively sums item in an array and returns the sum.
+ * @param {*} array 
+ */
+
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let count = 0;
+  for (item of array) {
+    if (Array.isArray(item)) { //Inductive step
+      count += sumItems(item);
+    } else { //Base case:
+      count += item;
+    }
+  }
+  return count;
+};
+
+console.log(sumItems([1, [1, 2, 3], 3, 4]));
 
 module.exports = sumItems;
